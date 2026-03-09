558b2d7052e56d33a349b42938125ba8fc617d5d
nguyendung113-cyber <nadung.hutech@gmail.com>
2026-03-09 20:03:01 +0700

    creat header

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -1,11 +1,14 @@
 // src/layouts/AdminLayout.tsx
+import Header from "../components/dashboard/Header.tsx/Header";
 import Sidebar from "../components/dashboard/Sidebar/Sidebar";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div style={{ display: "flex" }}>
       <Sidebar />
-      <main style={{ flex: 1, padding: "20px", backgroundColor: "#f8fafc" }}>
+      <main style={{ width: "80%", backgroundColor: "#f8fafc" }}>
+        <Header />
+
         {children}
       </main>
     </div>
